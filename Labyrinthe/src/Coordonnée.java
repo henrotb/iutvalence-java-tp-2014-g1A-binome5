6c39dc552033b4b14633bfbcd0ff9edbb325473e
Henrot <boris.henrot@iut-valence.fr>
2014-02-17 17:59:46 +0100

    Version 1.0 du labyrinthe

---
+++ Labyrinthe/src/Coordonnée.java
@@ -0,0 +1,12 @@
+
+public class Coordonnée 
+{
+    private int x;
+    private int y;
+    
+    public void Position()
+    {
+	this.x=1;
+	this.y=2;
+    }
+}
---
+++ Labyrinthe/src/Lab.java
@@ -0,0 +1,11 @@
+
+public class Lab 
+{
+    public static void main(String[] args) {
+	boolean tableau [][];
+	
+    }
+    private int niveau;
+
+	
+}
