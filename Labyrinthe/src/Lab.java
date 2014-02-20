32f798234c9b84c7f7454471b65b3a84c3d3241d
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-20 23:57:17 +0100

    Add TODO (Lab.java)

--- Labyrinthe/src/Lab.java
+++ Labyrinthe/src/Lab.java
@@ -2,10 +2,12 @@
 public class Lab 
 {
     public static void main(String[] args) {
+    /* TODO Pourquoi ici ? */
 	boolean tableau [][];
 	
     }
+    /* TODO Pourquoi ici ? */
     private int niveau;
 
-	
+
 }
