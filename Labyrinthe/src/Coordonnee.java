452577915913d46f34d1fb999d54a0cdc8acfd9c
Anthony Gelibert <anthony.gelibert@me.com>
2014-02-26 22:37:02 +0100

    Add TODO (Coordonnee.java)

--- Labyrinthe/src/Coordonnee.java
+++ Labyrinthe/src/Coordonnee.java
@@ -1,9 +1,11 @@
-
+/* TODO JavaDoc. */
+/* TODO Package. */
 public class Coordonnee 
 {
     private int x;
     private int y;
-    
+
+    /* TODO Qu'est-ce que c'est que ce truc ? */
     public void Position()
     {
 	this.x=x;
